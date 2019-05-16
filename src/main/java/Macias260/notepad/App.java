b4384f39c38410d2fa71d0f5f9b3f4d392a86307
MaciekM <mjmm260@gmail.com>
2019-05-16 19:15:20 +0200

    add maven project

---
+++ src/main/java/Macias260/notepad/App.java
@@ -0,0 +1,13 @@
+package Macias260.notepad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Macias260/notepad/AppTest.java
@@ -0,0 +1,20 @@
+package Macias260.notepad;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
